64029694f4ea00e8d43cfefc63b000ffdac7551a
cheived <cheived55@gmail.com>
2024-02-04 21:30:04 +0400

    logo path change

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -2,7 +2,7 @@ import './MainHeader.scss'
 function Header(props) {
     return (
         <div className="header">
-            <img className='logo' src={props.black ? "/coffee-shop/img/logoBlack.svg" : "/coffee-shop/img/Logo.svg"} alt="imageError" />
+            <img className='logo' src={props.black ? "/coffee-shop/img/logoBlack.svg" : "/coffee-shop/img/logo.svg"} alt="imageError" />
             <a style={{ color: props.black ? "black" : "white" }} href='localhost'>Our coffee</a>
             <a style={{ color: props.black ? "black" : "white" }} href='localhost'>For your pleasure</a>
 
